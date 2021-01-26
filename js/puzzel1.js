775363092e97836d0afbc2f7b64542e7e393903d
joeyyeoj <62258312+joeyyeoj@users.noreply.github.com>
2021-01-26 11:03:47 +0100

    Puzzel 1 functioneel af
    als omslachtige/hacky code een olympische sport was had ik minimaal 6 gouden medailles :D

--- js/puzzel1.js
+++ js/puzzel1.js
@@ -1,36 +1,95 @@
 window.onload = () =>{
   puzzleboxes = document.getElementsByClassName("js--puzzlebox");
+  puzzleslots = document.getElementsByClassName("js--puzzleslot")
   let camera = document.getElementById("js--camera");
   let scene = document.getElementById("js--scene");
   scene.add(camera)
   let hold = false;
-
+  resultArray = [false, false, false, false]
+  let placeCounter = 0
 
   for(let i=0; i<puzzleboxes.length; i++){
     puzzleboxes[i].addEventListener('click', function(){
       let pickedup = this
       if(!hold){
         if(!pickedup.id){
-            camera.innerHTML += '<a-box width="' + this.getAttribute("width") + '"' + 'depth="' + this.getAttribute("depth") + '"' + 'height="' + this.getAttribute("height") + '"' + 'color="' + this.getAttribute("color") + '"' + 'position="1 1 -2"' + 'rotation="180 180 0"' + '>' + pickedup.innerHTML + '</a-box>'
+            camera.innerHTML += '<a-box width="' + pickedup.getAttribute("width") + '"' + 'depth="' + pickedup.getAttribute("depth") + '"' + 'height="' + pickedup.getAttribute("height") + '"' + 'color="' + pickedup.getAttribute("color") + '"' + 'position="1 0.25 -2"' + 'rotation="180 160 0"' + '>' + pickedup.innerHTML + '</a-box>'
         }
         else{
           console.log(pickedup.getAttribute("color"))
           if(pickedup.getAttribute("color") == "yellow"){
-            camera.innerHTML += '<a-box '  + 'id="js--word1"' + 'width="' + this.getAttribute("width") + '"' + 'depth="' + this.getAttribute("depth") + '"' + 'height="' + this.getAttribute("height") + '"' + 'color="' + this.getAttribute("color") + '"' + 'position="1 1 -2"' + 'rotation="180 180 0"' + '>' + pickedup.innerHTML + '</a-box>'
+            camera.innerHTML += '<a-box '  + 'id="js--word1"' + 'width="' + pickedup.getAttribute("width") + '"' + 'depth="' + pickedup.getAttribute("depth") + '"' + 'height="' + pickedup.getAttribute("height") + '"' + 'color="' + pickedup.getAttribute("color") + '"' + 'position="1 0.25 -2"' + 'rotation="180 160 0"' + '>' + pickedup.innerHTML + '</a-box>'
           }
           else if(pickedup.getAttribute("color") == "cyan"){
-            camera.innerHTML += '<a-box '  + 'id="js--word2"' + 'width="' + this.getAttribute("width") + '"' + 'depth="' + this.getAttribute("depth") + '"' + 'height="' + this.getAttribute("height") + '"' + 'color="' + this.getAttribute("color") + '"' + 'position="1 1 -2"' + 'rotation="180 180 0"' + '>' + pickedup.innerHTML + '</a-box>'
+            camera.innerHTML += '<a-box '  + 'id="js--word2"' + 'width="' + pickedup.getAttribute("width") + '"' + 'depth="' + pickedup.getAttribute("depth") + '"' + 'height="' + pickedup.getAttribute("height") + '"' + 'color="' + pickedup.getAttribute("color") + '"' + 'position="1 0.25 -2"' + 'rotation="180 160 0"' + '>' + pickedup.innerHTML + '</a-box>'
           }
           else if(pickedup.getAttribute("color") == "purple"){
-            camera.innerHTML += '<a-box '  + 'id="js--word3"' + 'width="' + this.getAttribute("width") + '"' + 'depth="' + this.getAttribute("depth") + '"' + 'height="' + this.getAttribute("height") + '"' + 'color="' + this.getAttribute("color") + '"' + 'position="1 1 -2"' + 'rotation="180 180 0"' + '>' + pickedup.innerHTML + '</a-box>'
+            camera.innerHTML += '<a-box '  + 'id="js--word3"' + 'width="' + pickedup.getAttribute("width") + '"' + 'depth="' + pickedup.getAttribute("depth") + '"' + 'height="' + pickedup.getAttribute("height") + '"' + 'color="' + pickedup.getAttribute("color") + '"' + 'position="1 0.25 -2"' + 'rotation="180 160 0"' + '>' + pickedup.innerHTML + '</a-box>'
           }
           else if(pickedup.getAttribute("color") == "green"){
-            camera.innerHTML += '<a-box '  + 'id="js--word4"' + 'width="' + this.getAttribute("width") + '"' + 'depth="' + this.getAttribute("depth") + '"' + 'height="' + this.getAttribute("height") + '"' + 'color="' + this.getAttribute("color") + '"' + 'position="1 1 -2"' + 'rotation="180 180 0"' + '>' + pickedup.innerHTML + '</a-box>'
+            camera.innerHTML += '<a-box '  + 'id="js--word4"' + 'width="' + pickedup.getAttribute("width") + '"' + 'depth="' + pickedup.getAttribute("depth") + '"' + 'height="' + pickedup.getAttribute("height") + '"' + 'color="' + pickedup.getAttribute("color") + '"' + 'position="1 0.25 -2"' + 'rotation="180 160 0"' + '>' + pickedup.innerHTML + '</a-box>'
           }
         }
         hold = true
         }
       }
     )
+  }
+
+
+  for(let i=0; i<puzzleslots.length; i++){
+    puzzleslots[i].addEventListener('click', function(){
+      if(hold){
+        if(camera.childNodes[3].id){
+          if(camera.childNodes[3].getAttribute("color") == "yellow" && i==0){
+            resultArray[i] = true
+          }
+          if(camera.childNodes[3].getAttribute("color") == "cyan" && i==1){
+            resultArray[i] = true
+          }
+          if(camera.childNodes[3].getAttribute("color") == "purple" && i==2){
+            resultArray[i] = true
+          }
+          if(camera.childNodes[3].getAttribute("color") == "green" && i==3){
+            resultArray[i] = true
+          }
+        }
+        let box = document.createElement('a-box')
+        box.setAttribute("width", camera.childNodes[3].getAttribute("width"))
+        box.setAttribute("depth", camera.childNodes[3].getAttribute("depth"))
+        box.setAttribute("height", camera.childNodes[3].getAttribute("height"))
+        box.setAttribute("color", camera.childNodes[3].getAttribute("color"))
+        box.setAttribute("position", "0 -0.2 0")
+        box.innerHTML += camera.childNodes[3].innerHTML
+        this.appendChild(box)
+        camera.childNodes[3].remove()
+        hold=false
+        placeCounter+=1
+        if(placeCounter==4 && !resultArray.includes(false)){
+          console.log("Opgelost")
+          for(let i=0; i<puzzleslots.length; i++){
+            puzzleslots[i].childNodes[1].setAttribute("color", "green")
+
+            let book = document.createElement('a-box')
+            book.setAttribute("height", "0.2")
+            book.setAttribute("width", "0.4")
+            book.setAttribute("depth", "0.6")
+            book.setAttribute("position", "0 1 4")
+            scene.appendChild(book)
+          }
+        }
+        else if(placeCounter==4 && resultArray.includes(false)){
+          console.log("Fout")
+          for(let i=0; i<puzzleslots.length; i++){
+            puzzleslots[i].childNodes[1].setAttribute("color", "red")
+            setTimeout(function(){
+              puzzleslots[i].childNodes[3].remove()
+              puzzleslots[i].childNodes[1].setAttribute("color", "white")
+            }, 500)
+          }
+          placeCounter = 0
+        }
+      }
+    })
   }
 }
