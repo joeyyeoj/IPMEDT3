c19863c28f9ea6e4cd907aea453a8ffc0b12c88d
Jasper <s1122268@student.hsleiden.nl>
2021-01-27 16:23:16 +0100

    main.js Colors Removed
    - Colors verwijderd aangezien ze niet meer gebruikt werden, overbodige code dus.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,6 @@ window.onload = () =>{
   const camera = document.getElementById("js--camera");
   const cursor = document.getElementById("js--cursor");
 
-  const grey = "#7a7a7a";
-  const red = "#ff4646";
-  const yellow = "#ffe038";
-  const blue = "#2969ff";
-  const orange = "#fc9535";
-  const green = "#35fc5d";
-  const purple = "#bd35fc";
-
   this.setColor = function(object, color) {
     object.setAttribute("color", color);
   }
